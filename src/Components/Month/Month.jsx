a1548e7cb6a8409bd7c2ea788f07d2dc5a4bfa87
DmitriyKonnov <dimon170893@gmail.com>
2023-01-28 13:04:15 +0300

    изменил функциональные компаненты

--- src/Components/Month/Month.jsx
+++ src/Components/Month/Month.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Month({ title }) {
-  return <div className="w-full h-80 bg-elemnt rounded-xl">{title}</div>;
+  return (
+    <div className="w-full h-80 bg-elemnt rounded-xl">
+      <h3 className="text-center text-4xl">{title}</h3>
+    </div>
+  );
 }
