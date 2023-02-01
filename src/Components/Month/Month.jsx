0a0db05a7638f26a9510e3027c9e9840d8d782eb
DmitriyKonnov <dimon170893@gmail.com>
2023-02-01 22:54:45 +0300

    добил margin

--- src/Components/Month/Month.jsx
+++ src/Components/Month/Month.jsx
@@ -29,7 +29,7 @@ export const Month = ({ title }) => {
   return (
     <div className="w-full bg-elemnt rounded-xl p-2">
       <h3 className="text-center text-4xl m-2">{title}</h3>
-      <ul className="px-2 grid grid-cols-7 justify-items-center">{listDaysName}</ul>
+      <ul className="px-2 grid grid-cols-7 justify-items-center mb-5">{listDaysName}</ul>
       <ul className="grid grid-cols-7 justify-items-center w-full border rounded-xl border-font overflow-hidden">
         {listDays}
       </ul>
