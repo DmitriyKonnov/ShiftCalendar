b8cea0b9faea86c8d4bc56d92c9b954a2e90781e
DmitriyKonnov <dimon170893@gmail.com>
2023-02-09 19:09:09 +0300

    добавил изменение класса active по нажатию

--- src/Components/Month/Month.jsx
+++ src/Components/Month/Month.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const daysName = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 
@@ -16,22 +16,26 @@ const days = [
 ];
 
 export const Month = ({ title }) => {
-  const listDays = days.map((day, index) => {
-    return (
-      <li
-        className="border-r hover:bg-hover border-b border-font w-full text-center grid content-center aspect-square cursor-pointer"
-        key={index}>
-        {day}
-      </li>
-    );
-  });
+  const [addDay, setAddDay] = useState(false);
+  function onChangeDays() {
+    setAddDay(!addDay);
+  }
 
   return (
     <div className="w-full bg-elemnt rounded-xl p-2">
       <h3 className="text-center text-4xl m-2">{title}</h3>
       <ul className="px-2 grid grid-cols-7 justify-items-center mb-2">{listDaysName}</ul>
       <ul className="grid grid-cols-7 justify-items-center w-full border rounded-xl border-font overflow-hidden">
-        {listDays}
+        {days.map((day, index) => (
+          <li
+            onClick={onChangeDays}
+            className={`${
+              addDay === true ? 'bg-active' : ''
+            } border-r hover:bg-hover border-b border-font w-full text-center grid content-center aspect-square cursor-pointer`}
+            key={index}>
+            {day}
+          </li>
+        ))}
       </ul>
     </div>
   );
